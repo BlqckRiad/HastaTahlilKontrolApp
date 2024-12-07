e1e9044d750787509bf8d80c9b1922635e0fcf51
BlqckRiad <enesozbuganli36@gmail.com>
2024-12-07 11:30:44 +0300

    Proje kurulumu tamamlandı

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import LoginScreen from './assets/screens/LoginScreen';
+import RegisterScreen from './assets/screens/RegisterScreen'
+
+const App = () => {
+  return (
+  <LoginScreen />
+  );
+};
+
+export default App;
