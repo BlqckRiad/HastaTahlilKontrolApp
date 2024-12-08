8f90f761034b0b7e4134fdcc5f6ba3eaa7fa314e
BlqckRiad <enesozbuganli36@gmail.com>
2024-12-08 11:34:36 +0300

    COLORS AND SPACING UPDATED

--- App.js
+++ App.js
@@ -2,9 +2,11 @@ import React from 'react';
 import LoginScreen from './assets/screens/LoginScreen';
 import RegisterScreen from './assets/screens/RegisterScreen'
 
+import HomeScreen from './assets/screens/ScreensForUser/HomeScreen';
+
 const App = () => {
   return (
-  <LoginScreen />
+  <HomeScreen />
   );
 };
 
