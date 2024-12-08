95e5c754dfd571236be71bcf2d85aad08fbd0c04
YusufFurkanGunes <104029996+YusufFurkanGunes@users.noreply.github.com>
2024-12-08 11:39:43 +0300

    news data eklendi

---
+++ assets/data/News.js
@@ -0,0 +1,45 @@
+// News.js
+const News = [
+  {
+    Id: 1,
+    HaberImage:
+      "https://media.istockphoto.com/id/178382075/tr/foto%C4%9Fraf/hospital-surgery-corridor.jpg?s=612x612&w=0&k=20&c=jljtc3iDKf3pYCLw8J1_DW1Kr4zcmhXw8qftZt7-t7s=",
+    HaberTitle: "Yeni Hastane Açılışı",
+    HaberDesc:
+      "Şehir merkezinde modern teknolojiyle donatılmış yeni bir hastane açıldı. Hastane, yılda 500.000 hastaya hizmet verecek kapasitede.",
+  },
+  {
+    Id: 2,
+    HaberImage:
+      "https://media.istockphoto.com/id/178382075/tr/foto%C4%9Fraf/hospital-surgery-corridor.jpg?s=612x612&w=0&k=20&c=jljtc3iDKf3pYCLw8J1_DW1Kr4zcmhXw8qftZt7-t7s=",
+    HaberTitle: "Doktorlar İçin Yeni Eğitim Programı",
+    HaberDesc:
+      "Sağlık Bakanlığı, doktorlar için ileri düzey cerrahi eğitim programı başlattı. Bu program, daha güvenli ve etkili ameliyatlar yapılmasını hedefliyor.",
+  },
+  {
+    Id: 3,
+    HaberImage:
+      "https://media.istockphoto.com/id/178382075/tr/foto%C4%9Fraf/hospital-surgery-corridor.jpg?s=612x612&w=0&k=20&c=jljtc3iDKf3pYCLw8J1_DW1Kr4zcmhXw8qftZt7-t7s=",
+    HaberTitle: "Grip Salgını Uyarısı",
+    HaberDesc:
+      "Bu yıl grip vakalarında ciddi bir artış bekleniyor. Uzmanlar, vatandaşları grip aşısı yaptırmaya çağırıyor.",
+  },
+  {
+    Id: 4,
+    HaberImage:
+      "https://media.istockphoto.com/id/178382075/tr/foto%C4%9Fraf/hospital-surgery-corridor.jpg?s=612x612&w=0&k=20&c=jljtc3iDKf3pYCLw8J1_DW1Kr4zcmhXw8qftZt7-t7s=",
+    HaberTitle: "Kanser Araştırmalarında Yeni Umut",
+    HaberDesc:
+      "Bilim insanları, erken teşhis ve tedavi için yeni bir yöntem geliştirdi. Bu yöntem sayesinde kanser tedavisinde önemli adımlar atılabilir.",
+  },
+  {
+    Id: 5,
+    HaberImage:
+      "https://media.istockphoto.com/id/178382075/tr/foto%C4%9Fraf/hospital-surgery-corridor.jpg?s=612x612&w=0&k=20&c=jljtc3iDKf3pYCLw8J1_DW1Kr4zcmhXw8qftZt7-t7s=",
+    HaberTitle: "Sağlık Hizmetlerinde Dijitalleşme",
+    HaberDesc:
+      "Hastanelerde dijital randevu sistemi yaygınlaşıyor. Bu sistem sayesinde hastalar sıra beklemeden hizmet alabiliyor.",
+  },
+];
+
+export default News;
